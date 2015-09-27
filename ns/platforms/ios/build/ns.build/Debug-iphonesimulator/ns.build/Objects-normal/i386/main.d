973bf97ab6cdd9021fe984ad0d34e10fcd7d313a
Joshua Jensen <josh@pixelflavor.com>
2015-09-26 21:07:31 -0400

    Clean up before ignore

--- ns/platforms/ios/build/ns.build/Debug-iphonesimulator/ns.build/Objects-normal/i386/main.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/josh/Documents/Projects/mobilejs/ns/platforms/ios/ns/main.m \
-  /Users/josh/Documents/Projects/mobilejs/ns/platforms/ios/NativeScript/include/NativeScript.h \
-  /Users/josh/Documents/Projects/mobilejs/ns/platforms/ios/NativeScript/include/TNSRuntime.h \
-  /Users/josh/Documents/Projects/mobilejs/ns/platforms/ios/NativeScript/include/TNSRuntime+Inspector.h \
-  /Users/josh/Documents/Projects/mobilejs/ns/platforms/ios/ns/TNSDebugging.h
